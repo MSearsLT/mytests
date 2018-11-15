be0e01dacccc455cf8f0c406b9d430e96937ac5f
Matt Sears <msears@leveragingTechnology.com>
2018-11-15 12:53:02 -0500

    made update to new feature

--- Program.cs
+++ Program.cs
@@ -9,7 +9,8 @@ class Program
 #endif  
 
         Console.WriteLine("Demo program for understanding how MSBuld works. Thanks for playing!");
-        Console.WriteLine("I've added this great feature!");   
+        Console.WriteLine("I've added this great feature!");
+        Console.WriteLine("I've made an update to the new feature");   
         Console.Read();
     }  
 }  
