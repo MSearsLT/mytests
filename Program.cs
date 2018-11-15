0a7469b19b5947db4be9040988e1ef8192cc8f65
Matt Sears <msears@leveragingTechnology.com>
2018-11-15 12:50:51 -0500

    added new feature code

--- Program.cs
+++ Program.cs
@@ -8,7 +8,8 @@ class Program
         Console.WriteLine("WE ARE IN THE DEBUG CONFIGURATION");  
 #endif  
 
-        Console.WriteLine("Demo program for understanding how MSBuld works. Thanks for playing!");  
+        Console.WriteLine("Demo program for understanding how MSBuld works. Thanks for playing!");
+        Console.WriteLine("I've added this great feature!");   
         Console.Read();
     }  
 }  
